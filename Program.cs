d286aa148e3f19223b3d606527a4ab5cc2679112
Mario Antonio Castillo Enríquez <marioenriquez650@gmail.com>
2023-03-31 15:31:56 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ModeloDual_NET_Framework
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// Punto de entrada principal para la aplicación.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Conexion con = new Conexion();
+
+            //Console.WriteLine("El servidor es: ");
+            //Console.WriteLine(con.retornarMensaje());
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
